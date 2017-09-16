ad27c10078842743fe17ab24c609d1ce78ee55a0
BenVuti <theo.benven@gmail.com>
2017-09-16 16:14:37 +0200

    Add BinaryIndexedTree implementation with basic tests

---
+++ DataStructures/DataStructures/BIT/BinaryIndexedTree.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.BIT {
+    /// <summary>
+    /// Implementation of a Binary Indexed Tree (Fennwick Tree)
+    /// </summary>
+    public class BinaryIndexedTree {
+
+        private List<int> _list;
+
+        /// <summary>
+        /// Initialize BIT with list in O(n)
+        /// </summary>
+        public BinaryIndexedTree(IList<int> list) {
+            _list = new List<int>();
+            _list.Add(0);
+            _list.AddRange(list);
+
+            for (int index = 1; index < _list.Count; index++) {
+                int index2 = index + (index & -index);
+                if (index2 < _list.Count) {
+                    _list[index2] += _list[index];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Computes prefix sum of up to including the indexTo element
+        /// </summary>
+        public int SumTo(int indexTo) {
+            int result = 0;
+            for (++indexTo; indexTo > 0; indexTo -= indexTo & -indexTo) {
+                result += _list[indexTo];
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Computes the range sum between two indices (both inclusive)
+        /// </summary>
+        public int SumRange(int from, int to) {
+            return SumTo(to) - SumTo(from - 1);
+        }
+
+        /// <summary>
+        /// Add a value to the index element
+        /// </summary>
+        public void Update(int index, int add) {
+            for (++index; index < _list.Count; index += index & -index) {
+                _list[index] += add;
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,39 @@
+﻿using DataStructures.BIT;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructures {
+    class Program {
+        static void Main(string[] args) {
+
+            List<int> list = new List<int> { 1, 7, 3, 0, 5, 8, 3, 2, 6, 2, 1, 1, 4, 5 };
+            BinaryIndexedTree bit = new BinaryIndexedTree(list);
+
+            Console.WriteLine("Array: ");
+            Console.WriteLine($"{String.Join(" ,", list)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Prefix sum of first 13 elements: {bit.SumTo(12)}");
+            Console.WriteLine($"Prefix sum of first 7 elements: {bit.SumTo(6)}");
+            Console.WriteLine($"Range sum from pos 1 to pos 5: {bit.SumRange(1, 5)}");
+            Console.WriteLine();
+
+            bit.Update(4, 2);
+            Console.WriteLine("Add 2 to element at pos 4");
+            for (int i = 0; i < list.Count; i++) {
+                list[i] = bit.SumRange(i, i);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Array: ");
+            Console.WriteLine($"{String.Join(" ,", list)}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Prefix sum of first 13 elements: {bit.SumTo(12)}");
+            Console.WriteLine($"Prefix sum of first 7 elements: {bit.SumTo(6)}");
+            Console.WriteLine($"Range sum from pos 1 to pos 5: {bit.SumRange(1, 5)}");
+
+            Console.ReadKey();
+        }
+    }
+}
